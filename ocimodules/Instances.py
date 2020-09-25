03ee7784680cc0c0acce997689362ef9882c57f3
T-Srikanth <51802305+T-Srikanth@users.noreply.github.com>
2020-09-25 18:42:26 +0530

    Update Instances.py
    added function to delete Cluster network

--- ocimodules/Instances.py
+++ ocimodules/Instances.py
@@ -3,6 +3,45 @@ import time
 
 WaitRefresh = 15
 
+def DeleteClusterNetworks(config, Compartments):
+    AllItems = []
+    object = oci.core.ComputeManagementClient(config)
+
+    print ("Getting all CLusterNetwork objects")
+    for Compartment in Compartments:
+        items = oci.pagination.list_call_get_all_results(object.list_cluster_networks, compartment_id=Compartment.id).data
+        for item in items:
+            if (item.lifecycle_state != "TERMINATED"):
+                AllItems.append(item)
+                print("- {} - {}".format(item.display_name, item.lifecycle_state))
+
+    itemsPresent = True
+
+    while itemsPresent:
+        count = 0
+        for item in AllItems:
+            try:
+                itemstatus = object.get_cluster_network(cluster_network_id=item.id).data
+                if itemstatus.lifecycle_state != "TERMINATED":
+                    if itemstatus.lifecycle_state != "TERMINATING":
+                        try:
+                            print ("Deleting: {}".format(itemstatus.display_name))
+                            object.terminate_cluster_network(cluster_network_id=itemstatus.id)
+                        except:
+                            print ("error trying to delete: {}".format(itemstatus.display_name))
+                    else:
+                        print("{} = {}".format(itemstatus.display_name, itemstatus.lifecycle_state))
+                    count = count + 1
+            except:
+                print ("error deleting {}, probably already deleted".format(item.display_name))
+        if count > 0 :
+            print ("Waiting for all Objects to be deleted...")
+            time.sleep(WaitRefresh)
+        else:
+            itemsPresent = False
+    print ("All Objects deleted!")
+
+
 def DeleteInstancePools(config, Compartments):
     AllItems = []
     object = oci.core.ComputeManagementClient(config)
