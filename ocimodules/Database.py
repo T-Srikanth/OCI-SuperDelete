2da2a87b3b3eae3dcc0c500768c767d83be08b97
T-Srikanth <51802305+T-Srikanth@users.noreply.github.com>
2020-11-27 20:07:52 +0530

    Update Database.py
    added method for AutonomousContainerDB and AutonomousExadataInfra deletion

--- ocimodules/Database.py
+++ ocimodules/Database.py
@@ -135,3 +135,79 @@ def DeleteDBBackups(config, Compartments):
         else:
             itemsPresent = False
     print ("All Objects deleted!")
+
+def DeleteAutonomousContainerDatabase(config, Compartments):
+    AllItems = []
+    object = oci.database.DatabaseClient(config)
+
+    print ("Getting all Autonomous Container Database objects")
+    for Compartment in Compartments:
+        items = oci.pagination.list_call_get_all_results(object.list_autonomous_container_databases, compartment_id=Compartment.id).data
+        for item in items:
+            if (item.lifecycle_state != "TERMINATED"):
+                AllItems.append(item)
+                print("- {} - {}".format(item.display_name, item.lifecycle_state))
+
+    itemsPresent = True
+
+    while itemsPresent:
+        count = 0
+        for item in AllItems:
+            try:
+                itemstatus = object.get_autonomous_container_database(autonomous_container_database_id=item.id).data
+                if itemstatus.lifecycle_state != "TERMINATED":
+                    if itemstatus.lifecycle_state != "TERMINATING":
+                        try:
+                            print ("Deleting: {}".format(itemstatus.display_name))
+                            object.terminate_autonomous_container_database(autonomous_container_database_id=itemstatus.id)
+                        except:
+                            print ("error trying to delete: {}".format(itemstatus.display_name))
+                    else:
+                        print("{} = {}".format(itemstatus.display_name, itemstatus.lifecycle_state))
+                    count = count + 1
+            except:
+                print ("error getting : {}".format(item.display_name))
+        if count > 0 :
+            print ("Waiting for all Objects to be deleted...")
+            time.sleep(60)
+        else:
+            itemsPresent = False
+    print ("All Objects deleted!")
+
+def DeleteAutonomousExadataInfrastructure(config, Compartments):
+    AllItems = []
+    object = oci.database.DatabaseClient(config)
+
+    print ("Getting all Autonomous Exadata Infrastructure objects")
+    for Compartment in Compartments:
+        items = oci.pagination.list_call_get_all_results(object.list_autonomous_exadata_infrastructures, compartment_id=Compartment.id).data
+        for item in items:
+            if (item.lifecycle_state != "TERMINATED"):
+                AllItems.append(item)
+                print("- {} - {}".format(item.display_name, item.lifecycle_state))
+
+    itemsPresent = True
+
+    while itemsPresent:
+        count = 0
+        for item in AllItems:
+            try:
+                itemstatus = object.get_autonomous_exadata_infrastructure(autonomous_exadata_infrastructure_id=item.id).data
+                if itemstatus.lifecycle_state != "TERMINATED":
+                    if itemstatus.lifecycle_state != "TERMINATING":
+                        try:
+                            print ("Deleting: {}".format(itemstatus.display_name))
+                            object.terminate_autonomous_exadata_infrastructure(autonomous_exadata_infrastructure_id=itemstatus.id)
+                        except:
+                            print ("error trying to delete: {}".format(itemstatus.display_name))
+                    else:
+                        print("{} = {}".format(itemstatus.display_name, itemstatus.lifecycle_state))
+                    count = count + 1
+            except:
+                print ("error getting : {}".format(item.display_name))
+        if count > 0 :
+            print ("Waiting for all Objects to be deleted...")
+            time.sleep(WaitRefresh)
+        else:
+            itemsPresent = False
+    print ("All Objects deleted!")
