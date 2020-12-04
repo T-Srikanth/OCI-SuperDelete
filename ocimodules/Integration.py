c8431ae5e3cd2aa95ce42a0c1dfd14df059f5062
T-Srikanth <51802305+T-Srikanth@users.noreply.github.com>
2020-12-05 00:11:58 +0530

    Update Integration.py
    Added Data Integration deletion method

--- ocimodules/Integration.py
+++ ocimodules/Integration.py
@@ -40,3 +40,41 @@ def DeleteIntegrationInstances(config, Compartments):
         else:
             itemsPresent = False
     print ("All Objects deleted!")
+
+def DeleteDataIntegrationInstances(config, Compartments):
+    AllItems = []
+    object = oci.data_integration.DataIntegrationClient(config)
+
+    print ("Getting all Data Integration Instances")
+    for Compartment in Compartments:
+        items = oci.pagination.list_call_get_all_results(object.list_workspaces, compartment_id=Compartment.id).data
+        for item in items:
+            if (item.lifecycle_state != "DELETED"):
+                AllItems.append(item)
+                print("- {} - {}".format(item.display_name, item.lifecycle_state))
+
+    itemsPresent = True
+
+    while itemsPresent:
+        count = 0
+        for item in AllItems:
+            try:
+                itemstatus = object.get_workspace(workspace_id=item.id).data
+                if itemstatus.lifecycle_state != "DELETED":
+                    if itemstatus.lifecycle_state != "DELETING":
+                        try:
+                            print ("Deleting: {}".format(itemstatus.display_name))
+                            object.delete_workspace(workspace_id=itemstatus.id,quiesce_timeout=0,is_force_operation=True)
+                        except Exception as e:
+                            print ("error trying to delete: {}".format(itemstatus.display_name))
+                    else:
+                        print("{} = {}".format(itemstatus.display_name, itemstatus.lifecycle_state))
+                    count = count + 1
+            except:
+                print ("error getting : {}".format(item.display_name))
+        if count > 0 :
+            print ("Waiting for all Objects to be deleted...")
+            time.sleep(WaitRefresh)
+        else:
+            itemsPresent = False
+    print ("All Objects deleted!")
