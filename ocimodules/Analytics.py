4ac304f606f96a41ef569815b22195dfdeaad50e
T-Srikanth <51802305+T-Srikanth@users.noreply.github.com>
2020-12-04 23:08:15 +0530

    Add files via upload
    Adding modules for Analytics and Integration Instance deletion

---
+++ ocimodules/Analytics.py
@@ -0,0 +1,42 @@
+import oci
+import time
+
+WaitRefresh = 15
+
+def DeleteAnalyticsInstances(config, Compartments):
+    AllItems = []
+    object = oci.analytics.AnalyticsClient(config)
+
+    print ("Getting all Analytics Instances")
+    for Compartment in Compartments:
+        items = oci.pagination.list_call_get_all_results(object.list_analytics_instances, compartment_id=Compartment.id).data
+        for item in items:
+            if (item.lifecycle_state != "DELETED"):
+                AllItems.append(item)
+                print("- {} - {}".format(item.name, item.lifecycle_state))
+
+    itemsPresent = True
+
+    while itemsPresent:
+        count = 0
+        for item in AllItems:
+            try:
+                itemstatus = object.get_analytics_instance(analytics_instance_id=item.id).data
+                if itemstatus.lifecycle_state != "DELETED":
+                    if itemstatus.lifecycle_state != "DELETING":
+                        try:
+                            print ("Deleting: {}".format(itemstatus.name))
+                            object.delete_analytics_instance(analytics_instance_id=itemstatus.id)
+                        except Exception as e:
+                            print ("error trying to delete: {}".format(itemstatus.name))
+                    else:
+                        print("{} = {}".format(itemstatus.name, itemstatus.lifecycle_state))
+                    count = count + 1
+            except:
+                print ("error getting : {}".format(item.name))
+        if count > 0 :
+            print ("Waiting for all Objects to be deleted...")
+            time.sleep(WaitRefresh)
+        else:
+            itemsPresent = False
+    print ("All Objects deleted!")
